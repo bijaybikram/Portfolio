8bf8014e075d574852b5396f8953ea061f9b1d75
Bijay Bikram <becauseiam33@gmail.com>
2023-06-10 10:26:34 +0545

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+import App from "./components/App";
+
+const el = document.getElementById("root");
+
+const root = ReactDOM.createRoot(el);
